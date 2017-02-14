d51423318777aa3176630ac2ccb49938d4b1ca92
rohjan <thiagu.rohini@gmail.com>
2017-02-14 22:51:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 // Move the image
-var img = document.getImageById('madi');
+var img = document.getElementById('madi');
 img.onclick = function() {
     img.style.marginLeft='100px';    
 };
