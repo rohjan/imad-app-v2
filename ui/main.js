f3f94b8600389585cf73d3fccda6524448818f87
rohjan <thiagu.rohini@gmail.com>
2017-02-14 23:01:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,8 +6,14 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 // Move the image
 var img = document.getElementById('madi');
+var marginLeft=0;
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function() {
-    img.style.marginLeft='100px';    
+    //img.style.marginLeft='100px';
+    var interval = setInterval(moveRight,20);
 };
     
     
