eda769ce8f172105122916a0b3252fc02cd4f786
rohjan <thiagu.rohini@gmail.com>
2017-03-05 23:09:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,13 +89,13 @@ app.get('/', function (req, res) {
 
 function hash(input, salt) {
     //how do we create a hash
-    var hashed = crypto.pbkdf2Sync(input, salt, 100000, 512, 'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
 }
 
-var salt = 'this-is-some-random-string';
+//var salt = 'this-is-some-random-string';
 app.get('/hash/:input', function(req,res) {
-   var hashedString = hash(req.params.input, salt);
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
    res.send(hashedString);
 });
 var pool = new Pool(config);
