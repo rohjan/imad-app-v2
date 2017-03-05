65e2cf76f879ffc2da18946364a22ea46c3b38fc
rohjan <thiagu.rohini@gmail.com>
2017-03-05 23:02:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/', function (req, res) {
 
 function hash(input, salt) {
     //how do we create a hash
-    var hashed = crypto.pbkdf2Sync(input, salt, 100000, 512, sha512);
+    var hashed = crypto.pbkdf2Sync(input, salt, 100000, 512, 'sha512');
     return hashed.toString('hex');
 }
 
