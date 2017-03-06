9926c30044197143ade8dda4308914fccf50f3cf
rohjan <thiagu.rohini@gmail.com>
2017-03-07 00:05:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -172,7 +172,7 @@ app.get('/check-login', function(req, res) {
 });
 
 app.get('/logout', function(req,res) {
-    delete.req.session.auth;
+    delete req.session.auth;
     res.send('Logged out');
 });
 
