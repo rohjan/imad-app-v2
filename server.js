f6c32fec53e258bdd4bc7d5549f24742e071d4a3
rohjan <thiagu.rohini@gmail.com>
2017-03-06 19:16:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get('/hash/:input', function(req,res) {
    res.send(hashedString);
 });
 
-/*
+
 app.post('/create-user', function(req,res) {
    // username, password
    // {"username": "rohjan", "password": "password"}
@@ -120,7 +120,7 @@ app.post('/create-user', function(req,res) {
        
    });
 });
-*/
+
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) { 
     // make a select request
