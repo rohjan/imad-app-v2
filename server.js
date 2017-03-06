ba9774198313f952826d2cd059dc4d815e9ae25d
rohjan <thiagu.rohini@gmail.com>
2017-03-06 22:53:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-app.user(sesson({
+app.use(sesson({
     secret: 'someRandomSecretValue',
     cookie: {maxAge: 1000 * 60 * 60 * 24 * 30}
 }));
